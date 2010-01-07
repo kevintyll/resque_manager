a4580efee54a6c29e6924cc495b66e41b1696dc5
Kevin Tyll <kevintyll@gmail.com>
2010-01-07 11:05:08 -0500

    move assets into their own folder

--- install.rb
+++ install.rb
@@ -4,10 +4,14 @@ public_dir = File.join(File.dirname(__FILE__),'/../../../public')
 asset_dir = File.join(File.dirname(__FILE__), 'assets')
 
 ['images','stylesheets','javascripts'].each do |asset_type|
-  current_dir = File.join(asset_dir, asset_type)
-  Dir.entries(current_dir).each do |f|
-    path = File.join(public_dir,asset_type,f)
-    FileUtils.cp(File.join(asset_dir,asset_type,f), path) unless File.exist?(path) || File.directory?(path)
+  copy_from = File.join(asset_dir, asset_type)
+  copy_to = File.join(public_dir,asset_type,'resque')
+
+  FileUtils.mkdir(copy_to) unless File.exists? copy_to
+  Dir.entries(copy_from).each do |f|
+    new_file = File.join(copy_to,f)
+    from_file = File.join(copy_from,f)
+    FileUtils.cp(from_file, new_file) unless File.exist?(new_file) || File.directory?(from_file)
   end
 end
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
--- uninstall.rb
+++ uninstall.rb
@@ -1,11 +1,8 @@
 require 'fileutils'
 
 public_dir = File.join(File.dirname(__FILE__),'/../../../public')
-asset_dir = File.join(File.dirname(__FILE__), 'assets')
 ['images','stylesheets','javascripts'].each do |asset_type|
-  current_dir = File.join(asset_dir, asset_type)
-  Dir.entries(current_dir).each do |f|
-    path = File.join(public_dir,asset_type,f)
-    FileUtils.rm(path) if File.exist?(path) && File.file?(path)
-  end
+
+  path = File.join(public_dir,asset_type,'resque')
+  FileUtils.rm_r(path) if File.exist?(path)
 end
