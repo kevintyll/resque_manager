4e78c20c1c720072704a5dd58140b3082c74447a
Kevin Tyll <kevintyll@gmail.com>
2011-01-13 16:14:29 -0500

    add cap tasks to kill all resque workers on all servers

--- lib/resque_ui/tasks.rb
+++ lib/resque_ui/tasks.rb
@@ -4,22 +4,22 @@ namespace :resque do
   task :work => :setup do
     require 'resque'
 
-    worker = nil
-    queues = (ENV['QUEUES'] || ENV['QUEUE']).to_s.split('#').delete_if{|a| a.blank?}
-    threads = []
-    mqueue = queues.shift
+    worker                  = nil
+    queues                  = (ENV['QUEUES'] || ENV['QUEUE']).to_s.split('#').delete_if { |a| a.blank? }
+    threads                 = []
+    mqueue                  = queues.shift
     Thread.current[:queues] = mqueue
-    mworker = Resque::Worker.new(mqueue)
-    mworker.verbose = true #ENV['LOGGING'] || ENV['VERBOSE']
-    mworker.very_verbose = true #ENV['VVERBOSE']
+    mworker                 = Resque::Worker.new(mqueue)
+    mworker.verbose         = true #ENV['LOGGING'] || ENV['VERBOSE']
+    mworker.very_verbose    = true #ENV['VVERBOSE']
 
     queues.each do |queue|
       threads << Thread.new do
         begin
           Thread.current[:queues] = queue
-          worker = Resque::Worker.new(queue)
-          worker.verbose = ENV['LOGGING'] || ENV['VERBOSE']
-          worker.very_verbose = ENV['VVERBOSE']
+          worker                  = Resque::Worker.new(queue)
+          worker.verbose          = ENV['LOGGING'] || ENV['VERBOSE']
+          worker.very_verbose     = ENV['VVERBOSE']
         rescue Resque::NoQueueError
           abort "set QUEUE env var, e.g. $ QUEUE=critical,high rake resque:work"
         end
@@ -33,7 +33,7 @@ namespace :resque do
         end
       end
     end
-    threads.each {|thread| thread.join(0.5) }
+    threads.each { |thread| thread.join(0.5) }
   end
 
   desc "Restart all the workers"
@@ -43,6 +43,30 @@ namespace :resque do
     Resque.workers.sort_by { |w| w.to_s }.each do |worker|
       if pid != worker.pid
         worker.restart
+        pid = worker.pid
+      end
+    end
+  end
+
+  desc "Gracefully kill all the workers.  If the worker is working, it will finish before shutting down. arg: host=ip pid=pid"
+  task :quit_workers => :setup do
+    require 'resque'
+    pid = ''
+    Resque.workers.sort_by { |w| w.to_s }.each do |worker|
+      if pid != worker.pid
+        worker.quit
+        pid = worker.pid
+      end
+    end
+  end
+
+  desc "Kill all rogue workers on all servers.  If the worker is working, it will not finish and the job will go to the Failed queue as a DirtExit. arg: host=ip pid=pid"
+  task :kill_workers_with_impunity => :setup do
+    require 'resque'
+    pid = ''
+    Resque.workers.sort_by { |w| w.to_s }.each do |worker|
+      if pid != worker.pid
+        `kill -9 #{worker.pid}`
         pid = worker.pid
       end
     end
