0aeb93d244efb2467fc649d2aae44295cc7d12a3
Kevin Tyll <kevintyll@gmail.com>
2011-11-01 16:38:58 -0400

    == 3.1.5 2011-11-01
    *  Added the ability to set an optional :resque_rake variable in your deploy.rb file that is used by the resque:work task if set.

--- lib/resque_ui/cap_recipes.rb
+++ lib/resque_ui/cap_recipes.rb
@@ -3,17 +3,65 @@
 # ====================================
 
 
-#You must set the path to your rake task in your deploy.rb file.
+#You must set the path to your rake executable in your deploy.rb file.
 #ex.
 # set :rake, "/opt/ruby-enterprise-1.8.6-20090421/bin/rake"
+#Optionally, you can set another variable, :resque_rake.  This will be used for the resque:work task if set.
+#This allows you to set alternate options, particularly useful for jruby.
+#ex.
+#go through jexec so we get the right path and java settings
+# set :rake, "script/jexec rake"
+# set :resque_rake, "script/jexec -p rake" #extra memory settings for resque workers
+
+#the jexec file in script/ may look like:
+
+#scriptdir=`dirname $0`
+#
+## Hack to get things working under capistrano (used for torquebox to get rake working, still needed?)
+#if [ "$1" = "-j" ]; then
+#    shift
+#    export JRUBY_HOME=$1
+#    shift
+#fi
+#
+#if [ "$1" = "-m" ]; then
+#        shift
+#        export JAVA_MEM="-Xmx2048m -Xms256m -XX:PermSize=1024m -XX:MaxPermSize=1024m"
+#        export JAVA_STACK=-Xss4096k
+#fi
+#
+#export PATH=$scriptdir:/opt/jruby/bin:$PATH
+#
+## Configuration settings for all Rake Tasks and Resque Workers
+#if [ "$1" = "-p" ]; then
+#        shift
+#        # Set Heap Space for Young/Eden GC: 512MB
+#        # Initial Heap size: 2GB
+#        # Max Heap size: 4GB
+#        # Use server JVM
+#        # PermGenSize: 64MB
+#        # Max PermGenSize: 128MB
+#        # Thread Stack Size: 1024k
+#        export JRUBY_OPTS="-J-Xmn512m -J-Xms2048m -J-Xmx4096m -J-server -J-XX:PermSize=64m -J-XX:MaxPermSize=128m -J-Xss1024k"
+#        echo "JRUBY_OPTS=${JRUBY_OPTS}"
+#        nohup $* & #cap task runs in background
+#else
+#        exec $*
+#fi
+
 Capistrano::Configuration.instance(:must_exist).load do
+  
+  def get_rake
+    fetch(:resque_rake, fetch(:rake, "rake"))
+  end
+  
   namespace :resque do
     desc "start a resque worker. optional arg: host=ip queue=name"
     task :work, :roles => :resque do
       default_run_options[:pty] = true
       hosts                     = ENV['host'] || find_servers_for_task(current_task).collect { |s| s.host }
       queue                     = ENV['queue'] || '*'
-      rake                      = fetch(:rake, "rake")
+      rake                      = get_rake
       run("cd #{current_path}; nohup #{rake} RAILS_ENV=#{stage} QUEUE=#{queue} resque:work >> log/resque_worker.log 2>&1", :hosts => hosts)
     end
 
@@ -57,7 +105,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       hosts                     = ENV['host'] || find_servers_for_task(current_task).collect { |s| s.host }
       queue                     = ENV['queue'] || '*'
       count                     = ENV['count'] || '1'
-      rake                      = fetch(:rake, "rake")
+      rake                      = get_rake
       run("cd #{current_path}; nohup #{rake} RAILS_ENV=#{stage} COUNT=#{count} QUEUE=#{queue} resque:work >> log/resque_worker.log 2>&1", :hosts => hosts)
     end
 
--- resque_ui.gemspec
+++ resque_ui.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{resque_ui}
-  s.version = "3.1.4"
+  s.version = "3.1.5"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Kevin Tyll"]
-  s.date = %q{2011-10-27}
+  s.date = %q{2011-11-01}
   s.description = %q{A Rails UI for Resque for managing workers, failures and schedules.}
   s.email = %q{kevintyll@gmail.com}
   s.extra_rdoc_files = [
