e1aadae832cc0e2a915f3e12e266441adff51fdb
Kevin Tyll <kevintyll@gmail.com>
2013-07-30 14:56:31 -0400

    == 3.3.0 precompile assets

--- lib/resque_manager/engine.rb
+++ lib/resque_manager/engine.rb
@@ -1,5 +1,9 @@
 module ResqueManager
   class Engine < ::Rails::Engine
     isolate_namespace ResqueManager
+
+    initializer "resque_manager.assets.precompile" do |app|
+      app.config.assets.precompile += %w(resque_manager/application.css resque_manager/application.js)
+    end
   end
 end
